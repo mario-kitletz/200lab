670307c89d08065cccb5d56bb60d87eb4da54def
trunglnm <trunglnm@ponos-tech.com>
2021-02-23 15:33:46 +0700

    ~ build Page UI & navigate with nested_navigator

--- week_4/lib/pages/green_page.dart
+++ week_4/lib/pages/green_page.dart
@@ -1,4 +1,8 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:nested_navigators/nested_nav_bloc_provider.dart';
+import 'package:week_4/argument_key.dart';
+import 'package:week_4/nested_nav_item_key.dart';
+import 'package:week_4/routes.dart';
 
 class GreenPage extends StatelessWidget {
   final int value;
@@ -7,6 +11,69 @@ class GreenPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Green",
+        ),
+        backgroundColor: Colors.green,
+      ),
+      backgroundColor: Colors.green,
+      body: Container(
+        padding: EdgeInsets.all(16),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              value.toString(),
+              style: TextStyle(fontSize: 44, color: Colors.white),
+            ),
+            _divider(),
+            _item(
+              text: "open new page",
+              onPressed: () {
+                Navigator.of(context).pushNamed(Routes.green, arguments: {
+                  ArgumentKeys.value: value + 1,
+                });
+              },
+            ),
+            _divider(),
+            _item(
+              text: "select red tab",
+              onPressed: () {
+                final bloc = NestedNavigatorsBlocProvider.of(context);
+                bloc.select(NestedNavItemKey.red);
+              },
+            ),
+            _divider(),
+            _item(
+              text: "select blue tab",
+              onPressed: () {
+                NestedNavigatorsBlocProvider.of(context)
+                    .select(NestedNavItemKey.blue);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
+
+  _item({String text, Function() onPressed}) => TextButton(
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 16, color: Colors.white),
+        ),
+        onPressed: onPressed,
+      );
+
+  _divider() => Flexible(
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 8),
+          color: Colors.white,
+          height: 2,
+        ),
+      );
 }
--- week_4/lib/pages/red_page.dart
+++ week_4/lib/pages/red_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:nested_navigators/nested_nav_bloc_provider.dart';
+import 'package:nested_navigators/nested_navigators.dart';
+import 'package:week_4/argument_key.dart';
+import 'package:week_4/nested_nav_item_key.dart';
+import 'package:week_4/routes.dart';
 
 class RedPage extends StatelessWidget {
   final int value;
@@ -7,6 +13,108 @@ class RedPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('RED'),
+          backgroundColor: Colors.red,
+        ),
+        backgroundColor: Colors.red,
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(16),
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  value.toString(),
+                  style: TextStyle(fontSize: 44, color: Colors.white),
+                ),
+                _divider(),
+                _item(
+                    text: 'open new page',
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(Routes.red,
+                          arguments: {ArgumentKeys.value: value + 1});
+                    }),
+                _divider(),
+                _item(
+                    text: 'open new page and replace',
+                    onPressed: () {
+                      Navigator.of(context).pushReplacementNamed(Routes.red,
+                          arguments: {ArgumentKeys.value: value + 1});
+                    }),
+                _divider(),
+                _item(
+                    text: 'open new page and remove until first',
+                    onPressed: () {
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                          Routes.red, (route) => route.isFirst,
+                          arguments: {ArgumentKeys.value: value + 1});
+                    }),
+                _divider(),
+                _item(
+                    text: 'open new page and hide tab bar',
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(Routes.red, arguments: {
+                        ArgumentKeys.value: value + 1,
+                        hideNavTabBar: true,
+                      });
+                    }),
+                _divider(),
+                _item(
+                    text: 'select blue tab',
+                    onPressed: () {
+                      NestedNavigatorsBlocProvider.of(context)
+                          .select(NestedNavItemKey.blue);
+                    }),
+                _divider(),
+                _item(
+                    text: 'select green tab',
+                    onPressed: () {
+                      NestedNavigatorsBlocProvider.of(context)
+                          .select(NestedNavItemKey.green);
+                    }),
+                _divider(),
+                _item(
+                    text: 'select green tab and open new page 10',
+                    onPressed: () {
+                      NestedNavigatorsBlocProvider.of(context)
+                          .selectAndNavigate(
+                              NestedNavItemKey.green,
+                              (navigator) => navigator.pushNamed(Routes.green,
+                                  arguments: {ArgumentKeys.value: 10}));
+                    }),
+                _divider(),
+                _item(
+                  text: "open new page in root navigator",
+                  onPressed: () {
+                    Navigator.of(context, rootNavigator: true).pushNamed(
+                      Routes.red,
+                      arguments: {
+                        ArgumentKeys.value: value + 1,
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ));
   }
+
+  _item({String text, Function() onPressed}) => TextButton(
+      child: Text(
+        text,
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 16, color: Colors.white),
+      ),
+      onPressed: onPressed);
+
+  _divider() => Flexible(
+          child: Container(
+        margin: EdgeInsets.symmetric(vertical: 8),
+        color: Colors.white,
+        height: 2,
+      ));
 }
